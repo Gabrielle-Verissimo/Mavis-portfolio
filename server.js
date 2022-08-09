40196e8ebb94232baaebde3bf8f5ded1dd496347
Gabrielle Veríssimo <gabriellev007@gmail.com>
2022-08-09 19:15:35 -0300

    atualização backend

--- server.js
+++ server.js
@@ -5,18 +5,29 @@ const app = express();
 const path = require('path');
 const engine = require('consolidate');
 const nodemailer = require('nodemailer');
+const session = require('express-session');
 const flash = require('connect-flash');
 
-//async function sendMail()
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, 'public')));
 app.set('views', path.resolve(__dirname, 'views')); //arquivos renderizados na tela (HTML, EJS)
 app.engine('html', engine.mustache);
 app.set('view engine', 'html');
+app.use(session({
+    secret:'lasthope',
+    saveUninitialized: true,
+    resave: true
+}));
+app.use(flash());
 app.get('/', (req, res) => {
-    const name = 'Gabrielle'
-    return res.render('index', { name });
+    return res.render('index');
+});
+
+app.use((req, res, next) => {
+    res.locals.errors = req.flash('errors');
+    res.locals.success = req.flash('success');
+    next();
 });
 
 app.post('/send-message', async (req, res) => {
@@ -38,10 +49,8 @@ app.post('/send-message', async (req, res) => {
             html: `<p>${req.body.message}</p>`
         }
 
-        transporter.sendMail(message, () => {
-            console.log('enviado com sucesso');
-        })
-
+        transporter.sendMail(message);
+        req.flash('success', 'Mensagem enviada com sucesso');
         return res.redirect('/#contato');
     }
     catch(e){ console.log(e) }
