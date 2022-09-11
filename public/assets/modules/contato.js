0a06550191188678ded1213c44166e8e8c16ac11
Gabrielle Veríssimo <gabriellev007@gmail.com>
2022-09-11 18:24:46 -0300

    Atualização da validação do frontend

--- public/assets/modules/contato.js
+++ public/assets/modules/contato.js
@@ -1,22 +1,53 @@
-const name = document.getElementsByName('name').value;
-const email = document.getElementsByName('email').value;
-const message = document.getElementsByName('message').value;
+const form = document.querySelector('.form');
+const name = document.querySelector('input[name="name"]');
+const email = document.querySelector('input[name="email"]');
+const message = document.querySelector('#msg');
 
 export default function initValidator(){
-    formValidator();
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+        formValidator(e);
+    });
 }
 
-function formValidator(){
-    if(!name){
-        console.log('Preencha o campo Nome');
+function formValidator(e){
+    if(!name.value){
+        name.classList.add('error');
     }
-    if(!email){
-        console.log('Preencha o campo Email');
+    if(name.value && (name.value.length < 4)){
+        name.classList.add('error');
     }
-    if(email && validator.isEmail(email)){
-        console.log('Insira um email valido');
+    if(!email.value){
+        email.classList.add('error');
     }
-    if(!message){
-        console.log('Preencha o campo Mensagem');
+    if(email.value && !isEmail(email.value)){
+        email.classList.add('error');
     }
+    if(!message.value){
+        message.classList.add('error');
+    }
+
+    e.target.addEventListener('change', changeValidator());
+}
+
+function changeValidator(){
+    if(name.value){
+        name.classList.remove('error');
+    }
+    if(email.value && isEmail(email)){
+        email.classList.remove('error');
+    }
+}
+
+function isEmail(email){
+    let pattern = /.*@[^0-9.][a-z]*\..*/g;
+    let result = false;
+    if(pattern.exec(email)){
+        result = true;
+    }
+    else{
+        result = false;
+    }
+
+    return result;
 }
