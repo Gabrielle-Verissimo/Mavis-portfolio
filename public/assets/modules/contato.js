0868fd0351f7e90b799420238a76a612c5aef84c
Gabrielle Veríssimo <gabriellev007@gmail.com>
2022-09-12 11:23:58 -0300

    Atualização da validação do form. Frontend.

--- public/assets/modules/contato.js
+++ public/assets/modules/contato.js
@@ -11,32 +11,62 @@ export default function initValidator(){
 }
 
 function formValidator(e){
+    let error = 'Preenchimento de campo obrigatório';
     if(!name.value){
         name.classList.add('error');
+        (messageError(error, name));
     }
     if(name.value && (name.value.length < 4)){
         name.classList.add('error');
+        (messageError('Nome deve conter mais que 3 letras', name));
     }
     if(!email.value){
         email.classList.add('error');
+        messageError(error, email);
     }
     if(email.value && !isEmail(email.value)){
         email.classList.add('error');
+        messageError('Insira um email válido', email);
     }
     if(!message.value){
         message.classList.add('error');
+        messageError(error, message);
     }
 
     e.target.addEventListener('change', changeValidator());
 }
-
+//adicionar um evendo de click no botao enviar dentro de uma verificação se ainda existe erro
 function changeValidator(){
     if(name.value){
         name.classList.remove('error');
+        removeMessageError(name);
+    }
+    if(email.value){
+        email.classList.remove('error');
+        removeMessageError(email);
     }
     if(email.value && isEmail(email)){
         email.classList.remove('error');
+        removeMessageError(email);
     }
+    if(message.value){
+        message.classList.remove('error');
+        removeMessageError(message);
+    }
+}
+
+function removeMessageError(input){
+    const span = input.parentNode.querySelector('span');
+    console.log(input.parentNode)
+    input.parentNode.removeChild(span);
+};
+
+function messageError(error, input){
+    const inputParent = input.parentNode;
+    const span = document.createElement('span');
+    span.classList = 'error-message';
+    span.innerHTML = error;
+    inputParent.appendChild(span);
 }
 
 function isEmail(email){
